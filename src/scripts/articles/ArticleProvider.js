b3c8a82bd03d3ce6e92fde01d4c45e8e993f3aa4
Aaron Gertler <aaron.gertler@gmail.com>
2020-09-24 13:26:16 -0500

    created get, use, save, and delete Articles functions

--- src/scripts/articles/ArticleProvider.js
+++ src/scripts/articles/ArticleProvider.js
@@ -2,9 +2,15 @@
 
 const eventHub = document.querySelector("body")
 
+const dispatchStateChangeEvent = () => {
+    const articleStageChangedEvent = new CustomEvent('articleStateChanged')
+
+    eventHub.dispatchEvent(articleStageChangedEvent)
+}
+
 let articles = []
 
-export const getNotes = () => {
+export const getArticles = () => {
     return fetch('http://localhost:8088/articles')
         .then(response => response.json())
         .then(parsedArticles => {
@@ -12,6 +18,28 @@ export const getNotes = () => {
         })
 }
 
-export const useNotes = () => {
+export const useArticles = () => {
     return articles.slice()
+}
+
+export const saveArticle = artObj => {
+    return fetch('http://localhost:8088/articles', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(artObj)
+    })
+        .then(() => {
+            return getArticles()
+        })
+        .then(dispatchStateChangeEvent)
+}
+
+export const deleteArticle = articleId => {
+    return fetch(`http://localhost:8088/articles/${noteId}`, {
+        method: 'DELETE'
+    })
+        .then(getArticles)
+        .then(dispatchStateChangeEvent)
 }
