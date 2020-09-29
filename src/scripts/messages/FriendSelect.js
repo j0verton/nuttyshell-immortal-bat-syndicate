714261125023991e62043f5cd8d20ece8fa3a9c9
j0verton <overtonje@gmail.com>
2020-09-29 12:00:06 -0500

    removed a console.log

--- src/scripts/messages/FriendSelect.js
+++ src/scripts/messages/FriendSelect.js
@@ -1,12 +1,13 @@
-
+import { getFriends, useFriends } from "../friends/FriendsData.js";
+const eventHub = document.querySelector("body")
 const render = friendCollection => {
-    contentTarget.innerHTML = `
+    document.querySelector("#newMessageForm").innerHTML = `
         <select class="dropdown" id="friendSelect">
-            <option value="0">message all users...</option>
+            <option value="0">choose a friend To message...</option>
             ${
                 friendCollection.map(
                     friendObj => {
-                        const name = friendObj.user.name;
+                        const name = friendObj.user.username;
                         return `<option value="${name}">${name}</option>`;
                     }
                 )
@@ -15,11 +16,20 @@ const render = friendCollection => {
     `
 }
 
-
 export const FriendSelect = () => {
-getFriends()
-.then(() => {
-const friendsArray = useFriends()
-render(friendsArray)
-})
+    getFriends()
+    .then(() => {
+        const friendsArray = useFriends()
+        render(friendsArray)
+    })
 }
+
+eventHub.addEventListener("keydown", event => {
+    if(event.code === 50){
+        console.log("@")
+        FriendSelect()
+    }
+
+} )
+
+document.addEventListener('keyup', logKey)
