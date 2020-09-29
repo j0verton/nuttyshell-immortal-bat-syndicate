26af9959dd47b1a6cba0f482660d94cd3379f620
Aaron Gertler <aaron.gertler@gmail.com>
2020-09-29 12:57:54 -0500

    initial commit & display username on article

--- src/scripts/articles/Article.js
+++ src/scripts/articles/Article.js
@@ -4,7 +4,7 @@ export const ArticleHTMLConverter = (artObj) => {
     return `
         <article class="article--container" id="article--${artObj.id}">
             <h3 class="article--title">${artObj.title}</h3>
-            <p class="article--user">Posted by: ${artObj.userId}</p>
+            <p class="article--user">Posted by: ${artObj.user.username}</p>
             <p class="article--timestamp">Date: ${new Date(artObj.date).toLocaleDateString('en-US')}</p>
 
             <p class="article--synopsis">${artObj.synopsis}</p>
